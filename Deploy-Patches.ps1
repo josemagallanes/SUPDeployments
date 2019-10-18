fe5ad8b8fd04dd970be221d8f61567540c9dff18
Jose Magallanes <jose-magallanes@live.com>
2019-10-18 09:11:43 -0500

    Initial Upload

---
+++ Deploy-Patches.ps1
@@ -0,0 +1,145 @@
+# Get the initial location, so the user is returned at the end of the script run.
+$startlocation = Get-Location
+
+# Site configuration
+$SiteCode = "SL2" # Site code 
+$ProviderMachineName = "smsaus003.silabs.com" # SMS Provider machine name
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if($null -eq (Get-Module ConfigurationManager)) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+# Do not change anything above this line
+
+# Maintenance Window Constants
+$MW_DEV_COLLECTION = 'SL20007A'
+$MW_SAP_DEV_COLLECTION = 'SL20008A'
+$MW_QA_COLLECTION = 'SL20007C'
+$MW_SCCM_COLLECTION = 'SL20008C'
+$MW_EGL_COLLECTION = 'SL200081' 
+$MW_SAP_PROD_COLLECTION = 'SL200080'
+$MW_ADITS_COLLECTION = 'SL20008E'
+$MW_Z_FULL_PATCH_COLLECTION = 'SL20007F'
+
+# Deployment Constants
+$EGL_SERVERS_COLLECTION = 'SL200078'
+$SUM_ADITS_SERVERS = 'SL20008F'
+$SAP_PRODUCTION_COLLECTION = 'SL20004B'
+$Z_FULL_PATCH_COLLECTION = 'SL20004F'
+
+# Import Get-PatchTuesday function and calculate
+. C:\temp\mw\Get-PatchTuesday.ps1
+$patch_tuesday = Get-PatchTuesday
+Write-Host 'Patch Tuesday:' $patch_tuesday
+$patch_tuesday = $patch_tuesday.ToUniversalTime()
+Write-Host 'Patch Tuesday UTC time:'$patch_tuesday
+
+# Calculate Dev Maintenance Window Time
+
+$dev_start_time = $patch_tuesday.AddDays(2).AddHours(5).AddMinutes(45)
+$dev_end_time = $dev_start_time.AddHours(4)
+
+# Calculate QA Maintenance Window Time
+$qa_start_time = $dev_start_time.AddDays(1)
+$qa_end_time = $qa_start_time.AddHours(4)
+
+# Calculate SCCM Maintenance Window Time
+$sccm_start_time = $dev_start_time.AddDays(2)
+$sccm_end_time = $sccm_start_time.AddHours(4)
+
+# Calculate EGL Maintenance Window Time
+$egl_start_time = $patch_tuesday.AddDays(10).AddHours(11)
+$egl_end_time = $egl_start_time.AddHours(4)
+
+# Calculate Production Maintenance Window Time
+$prod_start_time = $patch_tuesday.AddDays(11).AddHours(8)
+$prod_end_time = $prod_start_time.AddHours(4)
+
+# Create Month Header, for deployment naming
+$month = $patch_tuesday.ToString("MMMM").ToUpper().Substring(0,3)
+$softwareUpdateName = $header = $patch_tuesday.Year.ToString() + " " + `
+    $patch_tuesday.Month.ToString() + " " + $month
+$header = $softwareUpdateName + " - "
+Write-Host 'Header:' $header #DELETEME
+Write-Host 'Software Update Group:' $softwareUpdateName #DELETEME
+
+# Get Software Update Group
+$sup = Get-CMSoftwareUpdateGroup -Name $softwareUpdateName
+
+### Create Maintenance Windows ###
+$dev_schedule = New-CMSchedule -End $dev_end_time -Start $dev_start_time -Nonrecurring -IsUTC
+$qa_schedule = New-CMSchedule -End $qa_end_time -Start $qa_start_time -Nonrecurring -IsUTC
+$sccm_schedule = New-CMSchedule -End $sccm_end_time -Start $sccm_start_time -Nonrecurring -IsUTC
+$egl_schedule = New-CMSchedule -End $egl_end_time -Start $egl_start_time -Nonrecurring -IsUTC
+$prod_schedule = New-CMSchedule -End $prod_end_time -Start $prod_start_time -Nonrecurring -IsUTC
+
+
+# Dev MW
+#New-CMMaintenanceWindow -CollectionId $MW_DEV_COLLECTION -Name ($header + 'MW Dev Servers')  `
+#    -Schedule $dev_schedule -ApplyTo SoftwareUpdatesOnly -whatif
+
+# SAP DEV,QA,MISC MW
+#New-CMMaintenanceWindow -CollectionId $MW_SAP_DEV_COLLECTION -Name ($header + 'MW SAP Dev,QA,Misc Servers') `
+#    -Schedule $dev_schedule -ApplyTo SoftwareUpdatesOnly -whatif
+
+# QA MW
+#New-CMMaintenanceWindow -CollectionId $MW_QA_COLLECTION -Name ($header + 'MW QA Servers') `
+#    -Schedule $qa_schedule -ApplyTo SoftwareUpdatesOnly -whatif
+
+# SCCM MW
+#New-CMMaintenanceWindow -CollectionId $MW_SCCM_COLLECTION -Name ($header + 'MW SUM SCCM') `
+#    -Schedule $sccm_schedule -ApplyTo SoftwareUpdatesOnly -whatif
+
+# EGL MW
+New-CMMaintenanceWindow -CollectionId $MW_EGL_COLLECTION -Name ($header + 'MW EGL Servers') `
+    -Schedule $egl_schedule -ApplyTo SoftwareUpdatesOnly
+
+#SAP PROD MW
+#New-CMMaintenanceWindow -CollectionId $MW_SAP_PROD_COLLECTION -Name ($header + 'MW SAP PROD Servers') `
+#    -Schedule $prod_schedule -ApplyTo SoftwareUpdatesOnly
+
+# AD/ITS MW
+#New-CMMaintenanceWindow -CollectionId $MW_ADITS_COLLECTION -Name ($header + 'MW AD/ITS Servers') `
+#    -Schedule $prod_schedule -ApplyTo SoftwareUpdatesOnly
+
+# Z-Full Collection MW
+#New-CMMaintenanceWindow -CollectionId $MW_Z_FULL_PATCH_COLLECTION -Name ($header + 'MW Z-Full Patch Servers') `
+#    -Schedule $prod_schedule -ApplyTo SoftwareUpdatesOnly
+
+#$desc = 'Patch Tuesday'
+
+<# UNCOMMENT NEXT PATCH CYCLE
+New-CMSoftwareUpdateDeployment -InputObject $sup -DeploymentName ($header + 'SCCM Test Servers') `
+    -Description $desc -DeploymentType Required -TimeBasedOn UTC -UserNotification DisplaySoftwareCenterOnly `
+    -RequirePostRebootFullScan $true -DownloadFromMicrosoftUpdate $true -DeadlineDateTime $prod_start_time `
+    -CollectionID 'SL200076' -UseBranchCache $true -ProtectedType RemoteDistributionPoint
+#>
+
+# EGL SUM Deployment
+New-CMSoftwareUpdateDeployment -InputObject $sup -DeploymentName ($header + 'EGL Servers') `
+    -DeploymentType Required -TimeBasedOn UTC -UserNotification DisplaySoftwareCenterOnly `
+    -RequirePostRebootFullScan $true -DownloadFromMicrosoftUpdate $true -DeadlineDateTime $egl_start_time `
+    -CollectionID $EGL_SERVERS_COLLECTION -UseBranchCache $true -ProtectedType RemoteDistributionPoint
+
+#New-CMSoftwareUpdateDeployment -SoftwareUpdateGroupName $sup -DeploymentName ($header + ' - SCCM Test Servers') `
+#    -Description 'Patch Tuesday' -DeploymentType Required -TimeBasedOn UTC -UserNotification DisplaySoftwareCenterOnly `
+#    -DeadlineDateTime $sccm_start_time -CollectionId 'SL200076' -whatif
+
+
+Set-Location $startlocation
---
+++ Get-PatchTuesday.ps1
@@ -0,0 +1,28 @@
+Function Get-PatchTuesday {
+<#
+    .SYNOPSIS
+    Get the DateTime for Microsoft's Patch Tuesday.
+    .DESCRIPTION
+    When run, finds the second Tuesday of the current month.
+#>
+
+$todays_date = Get-Date #For month reference
+$traversal_date = Get-Date -Date ($todays_date.Month.ToString() + '-01-' + `
+    $todays_date.Year.ToString())
+
+#For use when the first day of the week falls on Tuesday
+if ($traversal_date.DayOfWeek -eq 'Tuesday') {
+    $tuesday_i = 1;
+} else {
+    $tuesday_i = 0;
+} 
+
+while ($tuesday_i -ne 2) {
+    $traversal_date = $traversal_date.addDays(1)
+    if ($traversal_date.DayOfWeek -eq 'Tuesday') {
+        $tuesday_i = $tuesday_i + 1
+    }
+}
+$traversal_date
+}
+
