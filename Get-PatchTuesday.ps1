027d659f1e4d75c8d786d3484dd5c3e3c9c52751
Jose Magallanes <jose-magallanes@live.com>
2020-03-03 14:44:31 -0600

    Reformatted Get-PatchTuesday.ps1

--- Get-PatchTuesday.ps1
+++ Get-PatchTuesday.ps1
@@ -6,23 +6,23 @@ Function Get-PatchTuesday {
     When run, finds the second Tuesday of the current month.
 #>
 
-$todays_date = Get-Date #For month reference
-$traversal_date = Get-Date -Date ($todays_date.Month.ToString() + '-01-' + `
-    $todays_date.Year.ToString())
+    $todays_date = Get-Date # Gives us the current month for
+    $traversal_date = Get-Date -Date ($todays_date.Month.ToString() + '-01-' + `
+        $todays_date.Year.ToString())
 
-#For use when the first day of the week falls on Tuesday
-if ($traversal_date.DayOfWeek -eq 'Tuesday') {
-    $tuesday_i = 1;
-} else {
-    $tuesday_i = 0;
-} 
-
-while ($tuesday_i -ne 2) {
-    $traversal_date = $traversal_date.addDays(1)
+    #For use when the first day of the week falls on Tuesday
     if ($traversal_date.DayOfWeek -eq 'Tuesday') {
-        $tuesday_i = $tuesday_i + 1
-    }
-}
-$traversal_date
-}
+        $tuesday_i = 1;
+    } else {
+        $tuesday_i = 0;
+    } 
 
+    while ($tuesday_i -ne 2) {
+        $traversal_date = $traversal_date.addDays(1)
+        if ($traversal_date.DayOfWeek -eq 'Tuesday') {
+            $tuesday_i = $tuesday_i + 1
+        }
+    }
+    $traversal_date #Returned value
+}
+Get-PatchTuesday
